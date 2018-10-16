4273ce2a01977dcc0466e48804d7298a7910ef41
Tiago Souto <tiago.souto@gmail.com>
2018-10-16 13:58:14 -0300

    feat(core): improve components and add features

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import * as React from 'react';
-import { Helmet } from 'react-helmet';
+import * as React from 'react'
+import { Helmet } from 'react-helmet'
 
-import Routes from './router/routes';
+import Routes from './router/routes'
+import favicon from './assets/logo.png'
 
 class App extends React.Component {
   public render() {
     return (
       <React.Fragment>
         <Helmet>
-          <meta charSet='utf-8' />
-          <title>Frontend Project</title>
-          <link href='https://fonts.googleapis.com/icon?family=Material+Icons' rel='stylesheet' />
-          <meta name='description' content='Frontend Project' />
-          {/*
-          <link rel='icon' type='image/png' href={favicon} sizes='16x16' />
-          */}
+          <meta charSet="utf-8" />
+          <title>Super Digital</title>
+          <link
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+            rel="stylesheet"
+          />
+          <meta name="description" content="Super Digital" />
+
+          <link rel="icon" type="image/png" href={favicon} sizes="16x16" />
         </Helmet>
         <Routes />
       </React.Fragment>
-    );
+    )
   }
 }
 
-export default App;
+export default App
